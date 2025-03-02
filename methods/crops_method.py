03a340af82dec785b72175c95ad06b8dfdb50607
neerajanand321 <neerajanandfirst@gmail.com>
2025-03-02 19:21:40 +0530

    added qwen model

--- methods/crops_method.py
+++ methods/crops_method.py
@@ -1,6 +1,8 @@
 from methods.samplers.crops_sample import patch_crops_sampling
 from methods.model_forward.crops_llama_forward import patch_llama_forward
+from methods.model_forward.crops_qwen_forward import patch_qwen_forward
 
 def patch_everything():
     patch_crops_sampling()
     patch_llama_forward()
+    patch_qwen_forward()
--- methods/sid_method.py
+++ methods/sid_method.py
@@ -1,6 +1,8 @@
 from methods.samplers.sid_sample import patch_sid_sampling  
 from methods.model_forward.sid_llama_forward import patch_llama_forward
+from methods.model_forward.sid_qwen_forward import patch_qwen_forward
 
 def patch_everything():
     patch_sid_sampling()
     patch_llama_forward()
+    patch_qwen_forward()
