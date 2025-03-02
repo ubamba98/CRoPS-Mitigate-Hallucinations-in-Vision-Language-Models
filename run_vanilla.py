2f6717d7522bbb0fb4f8e2fdc2da1a111e4a5b00
Udbhav <ubamba98@gmail.com>
2025-03-02 14:53:57 +0530

    add vanilla sampler

--- run_vanilla.py
+++ run_vanilla.py
@@ -1,21 +1,3 @@
-from methods.crops_method import patch_everything
-patch_everything()
-
-from methods.generation_configs.crops_generation_config import GenerationConfigCRoPS
-
-from constants.crops_constants import (
-    DEFAULT_LAMBDA_LANG_PRIOR,  
-    DEFAULT_ALPHA_STAT_BIAS,
-    DEFAULT_BETA_CUTOFF,
-    DEFAULT_MAX_THRESHOLD_PLAUSIBILITY_CONSTRAINT,
-    DEFAULT_AGGREGATE_LAYER_FAST_V,
-    DEFAULT_MINUMUM_FAST_V_TOKENS,
-    DEFAULT_AGGREGATE_LAYER_TEXT_MASK,
-    DEFAULT_MINIMUM_TEXT_TOKENS
-)
-
-from constants.image_token_constants import BACKBONE_IMAGE_TOKEN_IDS
-
 from constants.default_generation_constants import (
     DEFAULT_MAX_NEW_TOKENS,
     DEFAULT_TEMPERATURE,
@@ -29,11 +11,10 @@ from utils.reproducibility_util import set_reproducibility
 import os
 import torch
 import argparse
-import numpy as np
 from tqdm.auto import tqdm
 from accelerate import PartialState
 from accelerate.utils import gather_object
-from transformers import AutoModelForImageTextToText, AutoProcessor
+from transformers import AutoModelForImageTextToText, AutoProcessor, GenerationConfig
 from transformers import logging
 logging.set_verbosity_error()
 
@@ -49,16 +30,6 @@ def args_parser():
     parser.add_argument("--temperature", type=float, default=DEFAULT_TEMPERATURE)
     parser.add_argument("--top_p", type=float, default=DEFAULT_TOP_P)
     parser.add_argument("--top_k", type=int, default=DEFAULT_TOP_K)
-
-    # CRoPS config
-    parser.add_argument("--lambda_lang_prior", type=float, default=DEFAULT_LAMBDA_LANG_PRIOR)
-    parser.add_argument("--alpha_stat_bias", type=float, default=DEFAULT_ALPHA_STAT_BIAS)
-    parser.add_argument("--beta_cutoff", type=float, default=DEFAULT_BETA_CUTOFF)
-    parser.add_argument("--max_threshold_plausibility_constraint", type=float, default=DEFAULT_MAX_THRESHOLD_PLAUSIBILITY_CONSTRAINT)
-    parser.add_argument("--aggregate_layer_fast_v", type=int, default=DEFAULT_AGGREGATE_LAYER_FAST_V)
-    parser.add_argument("--minumum_fast_v_tokens", type=int, default=DEFAULT_MINUMUM_FAST_V_TOKENS)
-    parser.add_argument("--aggregate_layer_text_mask", type=int, default=DEFAULT_AGGREGATE_LAYER_TEXT_MASK)
-    parser.add_argument("--minimum_text_tokens", type=int, default=DEFAULT_MINIMUM_TEXT_TOKENS)
 
     # Evaluation config
     parser.add_argument("--seed", type=int, default=42)
@@ -90,7 +61,7 @@ def main():
         run_chair_benchmark(model, processor, args)
 
 def run_chair_benchmark(model, processor, args):
-    experiment_name = os.path.join("experiments", "--".join(args.model_name.split("/")), "CRoPS")
+    experiment_name = os.path.join("experiments", "--".join(args.model_name.split("/")), "vanilla")
     os.makedirs(experiment_name, exist_ok=True)
 
     chair_benchmark = ChairBenchmarkDataset(
@@ -102,33 +73,6 @@ def run_chair_benchmark(model, processor, args):
     
     with distributed_state.local_main_process_first():
         test_dataset = chair_benchmark.get_test_dataset()
-
-    conversation_lang_prior = [
-            {
-                "role":"system",
-                "content": [
-                    {
-                        "type": "text", 
-                        "text": "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions."
-                    }
-                ]
-            },
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": "Please describe this image in detail"},
-                ],
-            },
-        ]
-
-    input_ids_lang_prior = processor.apply_chat_template(
-            conversation_lang_prior,
-            add_generation_prompt=True,
-            tokenize=True,
-            return_dict=True,
-            return_tensors="pt"
-        ).to(distributed_state.device, torch.bfloat16)
-    input_ids_lang_prior = input_ids_lang_prior["input_ids"]
 
     with distributed_state.split_between_processes(test_dataset) as process_test_dataset:
         generations = []
@@ -160,26 +104,11 @@ def run_chair_benchmark(model, processor, args):
                         return_tensors="pt"
                     ).to(distributed_state.device, torch.bfloat16)
 
-            image_token_ids = BACKBONE_IMAGE_TOKEN_IDS[args.model_name]
-            image_tokens = np.where(inputs["input_ids"].cpu().numpy()==image_token_ids)[1]
-            generation_config = GenerationConfigCRoPS(
+            generation_config = GenerationConfig(
                 max_new_tokens=args.max_new_tokens,
                 top_p=args.top_p,
                 temperature=args.temperature,
                 do_sample=args.do_sample,
-                key_position={
-                    "image_start": image_tokens[0],
-                    "image_end": image_tokens[-1],
-                },
-                input_ids_lang_prior=input_ids_lang_prior,
-                aggregate_layer_fast_v=args.aggregate_layer_fast_v,
-                minumum_fast_v_tokens=args.minumum_fast_v_tokens,
-                aggregate_layer_text_mask=args.aggregate_layer_text_mask,
-                minimum_text_tokens=args.minimum_text_tokens,
-                lambda_lang_prior=args.lambda_lang_prior,
-                alpha_stat_bias=args.alpha_stat_bias,
-                beta_cutoff=args.beta_cutoff,
-                max_threshold_plausibility_constraint=args.max_threshold_plausibility_constraint,
                 use_cache=True,
             )
 
